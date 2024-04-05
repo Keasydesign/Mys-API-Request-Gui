f8f7377fc74a6af2a39cef9084fe04d6e47b7afa
cartel <119452317+cartelfx@users.noreply.github.com>
2024-04-05 22:09:14 +0300

    Add files via upload

---
+++ types/inc/header_native.php
@@ -0,0 +1,11 @@
+<div class="page-content">
+    <div class="page-header">
+        <nav style="border: none !important;" class="navbar navbar-expand-lg d-flex justify-content-between">
+            <div class="header-title flex-fill">
+                <a href="#" id="sidebar-toggle"><i data-feather="arrow-left"></i></a>
+                <h5><?php echo $page_title ?></h5>
+            </div>
+            <div class="flex-fill" id="headerNav">
+            </div>
+        </nav>
+    </div>
---
+++ types/inc/header_sidebar.php
@@ -0,0 +1,23 @@
+﻿<style>
+    </style>
+<div class="page-container">
+    <div class="page-sidebar">
+        <ul class="list-unstyled accordion-menu">
+                 <div class="g-userbox pb-4 text-center" style="clear:left">
+                  <a href="/anasayfa" class="g-userarea text-center center">
+                  <div class="g-user-arrow ms-auto">
+                  </div>
+               </a>
+            </div>
+        
+            
+            </li>
+            </li>
+            <li><a title="Ezik Anora" href="../../index.php"><i class=""></i>Ana Sayfa</a></li>
+            <li><a title="Kullanıcı Sorgula" href="../../types/kullanıcı.php"><i class=""></i>Kullanıcı Bilgisi</a></li>
+            <li><a title="Yetkili Sorgula" href="../../types/yetkisorgu.php"><i class=""></i>  Yetkili Bilgisi</a></li>
+            <li><a title="Sunucu Sorgula" href="../../types/sunucu.php"><i class=""></i> Sunucu Bilgisi</a></li>
+            <li><a title="SonG Sorgula" href="../../types/sonGorulme.php"><i class=""></i>Son Görülme Bilgisi</a></li>
+            <li><a title="Stat Sorgula" href="../../types/stat.php"><i class=""></i>Stats Bilgisi</a></li>
+
+    </div>
